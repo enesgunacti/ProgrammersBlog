a744642f08945fac11caf7e125a1a531c72e72ce
enesg <gnctenes@gmail.com>
2021-10-22 19:13:09 +0300

    Fluent API öncesi son kısım

--- ProgrammersBlog.Shared/Data/Concrete/EntityFramework/EfEntityRepositoryBase.cs
+++ ProgrammersBlog.Shared/Data/Concrete/EntityFramework/EfEntityRepositoryBase.cs
@@ -22,34 +22,58 @@ namespace ProgrammersBlog.Shared.Data.Concrete.EntityFramework
             await _context.Set<TEntity>().AddAsync(entity);
         }
 
-        public async Task <bool> AnyAsync(Expression<Func<TEntity, bool>> predicate)
+        public async Task<bool> AnyAsync(Expression<Func<TEntity, bool>> predicate)
         {
             return await _context.Set<TEntity>().AnyAsync(predicate);
         }
 
-        public async Task <int> CountAsync(Expression<Func<TEntity, bool>> predicate)
+        public async Task<int> CountAsync(Expression<Func<TEntity, bool>> predicate)
         {
             return await _context.Set<TEntity>().CountAsync(predicate);
         }
 
         public async Task DeleteAsync(TEntity entity)
         {
-            await _context.Set<TEntity>().Remove(entity);
+            await Task.Run(() => { _context.Set<TEntity>().Remove(entity); });
         }
 
-        public async Task <IList<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> predicate = null, params Expression<Func<TEntity, object>>[] includeProperties)
+        public async Task<IList<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> predicate = null, params Expression<Func<TEntity, object>>[] includeProperties)
         {
-            throw new NotImplementedException();
+            IQueryable<TEntity> query = _context.Set<TEntity>();
+            if (predicate != null)
+            {
+                query = query.Where(predicate);
+            }
+            if (includeProperties.Any())
+            {
+                foreach (var includeProperty in includeProperties)
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+            return await query.ToListAsync();
         }
 
-        public async Task <TEntity> GetAsync(Expression<Func<TEntity, bool>> predicate, params Expression<Func<TEntity, object>>[] includeProperties)
+        public async Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> predicate, params Expression<Func<TEntity, object>>[] includeProperties)
         {
-            throw new NotImplementedException();
+            IQueryable<TEntity> query = _context.Set<TEntity>();
+            if (predicate != null)
+            {
+                query = query.Where(predicate);
+            }
+            if (includeProperties.Any())
+            {
+                foreach (var includeProperty in includeProperties)
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+            return await query.SingleOrDefaultAsync();
         }
 
         public async Task UpdateAsync(TEntity entity)
         {
-            throw new NotImplementedException();
+            await Task.Run(() => { _context.Set<TEntity>().Update(entity); });
         }
     }
 }
